4c581ee1c82878024ccb01f45d68d431d77cabea
Rafael Gandionco <rafaelgandi@gmail.com>
2021-08-06 19:15:56 +0800

    Version 0.20210806a

---
+++ WidgetMarkup.js
@@ -0,0 +1,183 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: cyan; icon-glyph: microscope;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// WidgetMarkup - Simple implementation of markup for Scriptable widgets.
+////////////////////////////////////////////////////////////////////////////////
+// Version 0.20210806a
+
+function _mapMethodsAndCall(inst, options) {
+    Object.keys(options).forEach((key) => {
+        if (key.indexOf('*') !== -1) {
+            key = key.replace('*', '');
+            if (Array.isArray(options['*' + key])) {
+                inst[key](...options['*' + key]);
+            }
+            else {
+                inst[key](options[key]);
+            }
+        }
+        else {
+            inst[key] = options[key];
+        }
+    });
+    return inst;
+}
+
+function _iterateChildren(widgetInstance, children) {
+    children.forEach((child) => {
+        if (child.tag === 'text') {
+            const holderKeyRegExp = /(\$\$\[.+\])/ig;
+            if (holderKeyRegExp.test(child.textContent)) {
+                child.textContent = child.textContent.replace(holderKeyRegExp, (match, p1) => {
+                    return holder[p1].toString();
+                });
+            }
+            let t = widgetInstance.addText(child.textContent);
+            _mapMethodsAndCall(t, _getAttrValue(child.attributes, 'styles'));
+        }
+        else if (child.tag === 'spacer') {
+            let space = parseInt(_getAttrValue(child.attributes, 'value'), 10);
+            if (space < 1 || isNaN(space)) {
+                widgetInstance.addSpacer();
+            }
+            else {
+                widgetInstance.addSpacer(space);
+            }
+        }
+        else if (child.tag === 'image') {
+            let img = widgetInstance.addImage(_getAttrValue(child.attributes, 'src'));
+            _mapMethodsAndCall(img, _getAttrValue(child.attributes, 'styles'));
+        }
+        else if (child.tag === 'date') {
+            let date = widgetInstance.addDate(_getAttrValue(child.attributes, 'value'));
+            _mapMethodsAndCall(date, _getAttrValue(child.attributes, 'styles'));
+        }
+        else if (child.tag === 'stack') {
+            let stack = widgetInstance.addStack();
+            _mapMethodsAndCall(stack, _getAttrValue(child.attributes, 'styles'));
+            _iterateChildren(stack, child.children);
+        }
+    });
+    return widgetInstance;
+}
+
+function _getAttrValue(attrs = [], name = 'styles') {
+    //console.log(attrs);
+    //console.log(name);
+    let attr = {};
+    attrs.forEach((a) => {
+        if (a.name.toLowerCase() === name.toLocaleLowerCase()) {
+            if (typeof holder[a.value] !== 'undefined') {
+                attr = holder[a.value];
+            }
+            else {
+                attr = a.value;
+            }
+        }
+    });
+    return attr;
+}
+
+const holder = {};
+function _replacer(str, eq) {
+    let builtStr = '';
+    str.forEach((s, i) => {
+        if (eq[i]) {
+            if (Array.isArray(eq[i])) {
+                eq[i] = eq[i].join('');
+            }
+            if (typeof eq[i] === 'string') {
+                //console.log(eq[i]);
+                builtStr += s + eq[i];
+            }
+            else {
+                let k = '$$[' + UUID.string() + (Math.floor(Math.random() * 20)) + ']';
+                holder[k] = eq[i];
+                builtStr += s + k;
+            }
+        }
+        else {
+            builtStr += s;
+        }
+
+    });
+    return builtStr;
+}
+
+function concatMarkup(str, ...eq) {
+    let r = _replacer(str, eq);
+    return r;
+}
+
+async function _getMappedDOM(markup) {
+    const webview = new WebView();
+    await webview.loadHTML('<html></html>');
+    //console.log(markup);
+    markup = markup.replace(/\n/ig, '');
+    markup = `<raffy-element>${markup}</raffy-element>`;
+    // See: https://gomakethings.com/how-to-create-a-map-of-dom-nodes-with-vanilla-js/
+    const js = `
+      var getAttributes = function (attributes) {
+      	return Array.prototype.map.call(attributes, function (attribute) {
+      		return {
+      			name: attribute.name,
+      			value: attribute.value
+      		};
+      	});
+      };
+      
+      var createDOMMap = function (element) {
+      	return Array.prototype.map.call(element.childNodes, (function (node) {
+          if (node.nodeType !== 3 && node.nodeType !== 8) {
+            var details = {
+                 tag: node.tagName.toLowerCase(),
+                 textContent: node.textContent,
+        			attributes: node.nodeType !== 1 ? [] : getAttributes(node.attributes)    			
+        		};
+        		details.children = createDOMMap(node);
+        		return details;
+          }
+          return null;   		
+      	}))
+        .filter((e) => e !== null);
+      };
+  
+      function getDom() {
+        let htmlStr = '${markup}';
+        const dom = new DOMParser();
+        let doc = dom.parseFromString(htmlStr, 'application/xml');
+        return JSON.stringify(createDOMMap(doc.documentElement));
+      }
+      getDom();
+  `;
+    let response = await webview.evaluateJavaScript(js, false);
+    return JSON.parse(response);
+}
+
+
+async function widgetMarkup(str, ...eq) {
+    let markup = _replacer(str, eq);
+    let map = await _getMappedDOM(markup);
+    const parentElementMap = map[0];
+    const childrenMap = parentElementMap.children;
+    const widget = new ListWidget();
+
+    _mapMethodsAndCall(widget, _getAttrValue(parentElementMap.attributes, 'styles'));
+    _iterateChildren(widget, childrenMap);
+    return widget;
+}
+
+
+
+module.exports.widgetMarkup = widgetMarkup;
+module.exports.concatMarkup = concatMarkup;
+
+
+
+
+
+
+
